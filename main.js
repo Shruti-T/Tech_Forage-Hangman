695048a81acda647e2d73f3e885bfcbfeed1b54a
Shruti Tiwari <sh03shruti@gmail.com>
2020-11-26 17:36:42 +0530

    Added files for Q\A section

--- main.js
+++ main.js
@@ -3,27 +3,44 @@ document.getElementById('submitbtn').style.display = 'none';
 document.getElementById('page2_view').style.display = 'none';
 
 var questions = ['what is my name?', 'where do you live?', 'what is your age?', 'where do you work?', 'what are your hobbies?'];
-
+//var hangman_Pics = ['../Tech_Forage-Hangman/Images/man_0.png', '../Tech_Forage-Hangman/Images/man_1.png', '../Tech_Forage-Hangman/Images/man_2.png', '../Tech_Forage-Hangman/Images/man_3.png', '../Tech_Forage-Hangman/Images/man_4.png']
 
 document.getElementById('loginbtn').onclick = function () {
+    var Name = document.getElementById('userName').value;
+    var Password = document.getElementById('password').value;
 
-    mode = 0;
-    serialJSON['name'] = document.getElementById('userName').value;
-    serialJSON['password'] = document.getElementById('password').value;
+    if (Name == '' || Password == '') {
+        document.getElementById('loginInstruction').innerHTML = 'Please fill in the details.';
+    }
+    else {
+        mode = 0;
+        serialJSON['name'] = Name;
+        serialJSON['password'] = Password;
 
-    successful(mode, serialJSON);
+        successful(mode, serialJSON);
+    }
     return false;
 };
 
+
 document.getElementById('submitbtn').onclick = function () {
-    console.log('works');
-    mode = 1;
-    serialJSON['name'] = document.getElementById('userName').value;
-    serialJSON['password'] = document.getElementById('password').value;
-    serialJSON['Answer'] = document.getElementById('Ans').value;
+    var Name = document.getElementById('userName').value;
+    var Password = document.getElementById('password').value;
+    var Answer = document.getElementById('Ans').value.toLowerCase().trim();
 
-    successful(mode, serialJSON);
+    if (Name == '' || Password == '' || Answer == '') {
+        document.getElementById('attemptsInstruction').innerHTML = 'Please fill in the details.';
+    }
+    else {
+        document.getElementById('attemptsInstruction').innerHTML = '';
+        mode = 1;
+        serialJSON['name'] = Name;
+        serialJSON['password'] = Password;
+        serialJSON['Answer'] = Answer;
 
+        successful(mode, serialJSON);
+    }
+    console.log(serialJSON);
     return false;
 }
 
@@ -48,15 +65,39 @@ function successful(mode) {
                 //document.getElementById('skipbtn').style.display = 'block';
                 console.log('wrong credentials');
                 document.getElementById('loginInstruction').innerHTML = 'wrong credentials';
+                //document.getElementById('attemptsInstruction').innerHTML = 'wrong credentials';
+
             }
-            else {
+            else if (res.ReturnedStatus == 1) {
                 console.log('correct credentials');
                 console.log(res);
-                document.getElementById('loginInstruction').innerHTML = '';
-                document.getElementById('loginbtn').style.display = 'none';
-                document.getElementById('skipbtn').style.display = 'inline';
-                document.getElementById('submitbtn').style.display = 'inline';
-                document.getElementById('page2_view').style.display = 'block';
+                if (mode == 0) {
+                    document.getElementById('Que').innerHTML = questions[res.current_question];
+                    document.getElementById('loginInstruction').innerHTML = '';
+                    document.getElementById('loginbtn').style.display = 'none';
+                    document.getElementById('skipbtn').style.display = 'inline';
+                    document.getElementById('submitbtn').style.display = 'inline';
+                    document.getElementById('page2_view').style.display = 'block';
+                    //pic...
+                    document.getElementById('hangman_pic').src = `../Tech_Forage-Hangman/Images/man_${res.Attempts}.png`;
+                }
+                else if (mode == 1) {
+                    if (res.Ans_Status == 1) //...correct answer
+                    {
+                        document.getElementById('Que').innerHTML = questions[res.current_question];
+                        document.getElementById('Ans').value = '';
+                    }
+                    else if (res.Ans_Status == 0)//else if  ..wrong answer
+                    {
+                        //pic update.
+                        document.getElementById('hangman_pic').src = `../Tech_Forage-Hangman/Images/man_${res.Attempts}.png`;
+                        document.getElementById('Ans').value = '';
+
+                    }
+
+                    console.log(res);
+                }
+
             }
 
         },
